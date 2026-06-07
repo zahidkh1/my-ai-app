2894472bae99d025b3b077bf377c600d37d5db9a
zahid khan <zahidkhan0719@gmail.com>
2026-06-07 09:10:51 +0530

    Trigger verbal deployment

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-// deployment trigger
+// deployment trigger v2
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
